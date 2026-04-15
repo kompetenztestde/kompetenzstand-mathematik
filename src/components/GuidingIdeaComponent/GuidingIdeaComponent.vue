da78ff0c5800fd1da5f0dc0eb40ccd2e7e22999f
Adrian <adr@outermedia.de>
2026-04-15 14:45:36 +0200

    TBA3-GuidingIdeas: styling added and texts from json used for page and description in side modal

--- src/components/GuidingIdeaComponent/GuidingIdeaComponent.vue
+++ src/components/GuidingIdeaComponent/GuidingIdeaComponent.vue
@@ -1,26 +1,49 @@
 <script setup lang="ts">
 import { useGuidingIdeas } from '@/composables/useGuidingIdeas'
 import styles from './styles.module.css'
+import layout from '@/assets/styles/component-layout.module.css'
 import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 import SingleBarChart from '../SingleBarChart/SingleBarChart.vue'
+import '@/assets/styles/base.css'
+import { useModalStore } from '@/stores/modalStore'
+
+const modalStore = useModalStore();
 
 const route = useRoute()
 const activeSubStep = computed(() => {
     const sId = route.params.subId
     return sId !== undefined ? Number(sId) : 0
 })
-const { topPerformers } = useGuidingIdeas('8b-mathe', 'students')
+const currentUserName = computed(() => route.query.user as string)
+
+const { topPerformers } = useGuidingIdeas(currentUserName)
 
 const currentItem = computed(() => topPerformers.value[activeSubStep.value])
+
+const showDetails = () => {
+    if (currentItem.value) {
+        modalStore.openModal(
+            currentItem.value.label, 
+            currentItem.value.description 
+        );
+    }
+};
 </script>
 
 <template>
-    <div v-if="currentItem" :class="styles.statPaginator">
-        <div :class="styles.card">
-            <h3>{{ currentItem.label }}</h3>
-            <SingleBarChart :percentage="currentItem.percentage" />
-            <span :class="styles.text">{{ currentItem.text }}</span>
+    <div v-if="currentItem" :class="styles.statPaginator" >
+        <div :class="layout.baseCard">
+            <div :class="[layout.baseIllustration, styles.illustrationHeader]">
+                <h2>{{ currentItem.label }}</h2>
+                <img @click="showDetails" :class="styles.info" src="@/themes/icons/info.svg"/>
+            </div>
+            <div :class="layout.baseContentArea">
+                <SingleBarChart :percentage="currentItem.percentage" />
+                <span class="text-body">
+                    {{ currentItem.text }}
+                </span>
+            </div>
         </div>
     </div>
 </template>
