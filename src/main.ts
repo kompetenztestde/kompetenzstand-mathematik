02545e5465b263a5ea8ec3cf0d59460f4be861b6
Adrian <adr@outermedia.de>
2026-04-15 14:31:44 +0200

    TBA3-I18N: locale file, declaration, file added to main

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { VueQueryPlugin } from '@tanstack/vue-query'
 
 import App from './App.vue'
 import router from './router'
+import i18n from './i18n';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(VueQueryPlugin)
+app.use(i18n);
 
 app.mount('#app')
