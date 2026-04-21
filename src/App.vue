2aa111d02f0f68ce05482eee93faa7c29f0fb620
Adrian <adr@outermedia.de>
2026-04-21 14:45:52 +0200

    TBA3: new functions used with new API and style module added

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,22 @@
 import { computed, watch } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import { useNavigation } from './composables/useNavigation'
-import { useCompetences } from './composables/useCompetences'
-import { useGuidingIdeas } from './composables/useGuidingIdeas'
 import './assets/styles/variables.css'
 import './assets/styles/base.css'
-import { useI18n } from 'vue-i18n';
+import { useI18n } from 'vue-i18n'
 import SideModal from './components/SideModal/SideModal.vue'
-
+import { useGuidingIdeasNew } from './composables/useGuidingIdeasNew'
+import { useCompetencesNew } from './composables/useCompetencesNew'
+import styles from './styles.module.css'
 const router = useRouter()
 const route = useRoute()
-const { t } = useI18n();
+const { t } = useI18n()
 
-const currentUserName = computed(() => route.query.user as string)
+const currentUserCode = computed(() => route.query.user as string)
 
-const { topPerformers: guidingIdeaTopPerformers, badPerformers } = useGuidingIdeas(currentUserName)
-const { topPerformers: competencesTopPerformers } = useCompetences(currentUserName)
+const { topPerformers: guidingIdeaTopPerformers, badPerformers, calculatedAreas, isLoading } = useGuidingIdeasNew(currentUserCode)
+const { topPerformers: competencesTopPerformers } = useCompetencesNew(currentUserCode)
+
 const { allSteps } = useNavigation(competencesTopPerformers, guidingIdeaTopPerformers, badPerformers)
 
 const currentIndex = computed(() => {
@@ -72,20 +73,6 @@ const goTo = (newIndex: number) => {
     })
 }
 
-watch(
-    () => route.fullPath,
-    (newPath) => {
-        console.log('Pfad:', newPath, 'Index:', currentIndex.value)
-    },
-)
-
-watch(
-    allSteps,
-    (newList) => {
-        console.log('Navigations-Liste aktualisiert. Einträge:', newList.length)
-    },
-    { immediate: true },
-)
 const isHome = computed(() => route.path === '/step-1' || route.path === '/')
 const isSecond = computed(() => route.path === '/step-2')
 
@@ -98,140 +85,43 @@ const appBackground = computed(() => {
 </script>
 
 <template>
-    <div class="appWrapper" :style="appBackground">
-        <main class="content" :class="{ 'no-padding': isHome || isSecond }">
+    <div :class="styles.appWrapper" :style="appBackground">
+        <!-- <main class="content" :class="(styles.content, { 'no-padding': isHome || isSecond })">
             <RouterView :key="route.fullPath" />
-        </main>
+        </main> -->
+        <main :class="[styles.content, { 'no-padding': isHome || isSecond }]">
+    <RouterView :key="route.fullPath" />
+</main>
 
         <SideModal />
 
-        <footer v-if="!isHome" class="navigationBar">
-            <h2 class="reportH1">{{t("home.feedback")}}</h2>
-            <button :disabled="currentIndex <= 0" @click="goBack" class="navBtn" aria-label="Zurück">
-                <img src="@/assets/svgs/page_left.svg" alt="" class="navIcon" />
+        <footer v-if="!isHome" :class="styles.navigationBar">
+            <h2 :class="styles.reportH1">{{ t('home.feedback') }}</h2>
+            <button :disabled="currentIndex <= 0" @click="goBack" :class="styles.navBtn" aria-label="Zurück">
+                <img src="@/assets/svgs/page_left.svg" alt="" :class="styles.navIcon" />
             </button>
-            <div class="pageIndicator">
+            <div :class="styles.pageIndicator">
                 <div
                     v-for="(step, index) in allSteps"
                     :key="index"
-                    class="dot"
-                    :class="{ active: index === currentIndex }"
+                    :class="`${styles.dot} ${index === currentIndex ? styles.active : ''}`"
                     @click="goTo(index)"
                 ></div>
             </div>
             <button
                 :disabled="currentIndex >= allSteps.length - 1 || currentIndex === -1"
                 @click="goNext"
-                class="navBtn next"
+                :class="[styles.navBtn, styles.next]"
                 aria-label="Weiter"
             >
-                <img src="@/assets/svgs/page_right.svg" alt="" class="navIcon" />
+                <img src="@/assets/svgs/page_right.svg" alt="" :class="styles.navIcon" />
             </button>
         </footer>
     </div>
 </template>
 <style>
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
-
-.appWrapper {
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-    background-color: var(--color-turquise);
-    transition: background 0.5s ease;
-}
-
-.content {
-    flex: 1;
-    /* margin-bottom: 10dvh; */
-}
-
-.navigationBar {
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    min-height: 10dvh;
-
-    background: var(--color-turquise);
-    border-top: 1px solid #ddd;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    /* padding: 15px 20px 15px 20px; */
-    box-shadow: 0 -2px 10px rgba(0, 0, 0, 0.05);
-}
-
-.navBtn {
-    cursor: pointer;
-    background: none;
-    border: none;
-    padding: 0;
-}
-
-.navBtn:disabled {
-    opacity: 0.3;
-    cursor: not-allowed;
-}
-
-@media (min-width: 768px) {
-    .navBtn {
-        padding: 15px 40px;
-    }
-    .navigationBar{
-        padding: 0px 20px 0px 20px;
-    }
-}
-
-@media (max-width: 900px) {  
-    .reportH1{
-        display:none
-    }
-}
-
-.pageIndicator {
-    display: flex;
-    gap: 12px;
-    align-items: center;
-}
-
-.dot {
-    width: 10px;
-    height: 10px;
-    background-color: var(--color-navigation-blue);
-    border-radius: 50%;
-    transition: all 0.3s ease;
-    position: relative;
-}
-
-.dot.active {
-    background-color: var(--color-navigation-blue);
-    transform: scale(1.2);
-}
-
-.dot.active::after {
-    content: '';
-    position: absolute;
-    top: -4px;
-    left: -4px;
-    right: -4px;
-    bottom: -4px;
-    background-color: var(--color-white);
-    border-radius: 50%;
-    border: 1px solid var(--color-navigation-blue);
-    opacity: 0.5;
-}
-
-.dot:hover {
-    background-color: #bbb;
-    transform: scale(1.1);
-}
-
 .appWrapper .content.no-padding {
     padding-bottom: 0 !important;
+    min-height: calc(100dvh - 15dvh - 40px);
 }
 </style>
