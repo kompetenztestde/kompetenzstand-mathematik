1b99e1db736a4302dfe25472d4468020c619ec29
Viktor Stefanko <viktor.stefanko@uni-jena.de>
2026-09-04 15:39:27 +0200

    Config für dev mode angepasst

--- public/config.js
+++ public/config.js
@@ -1,7 +1,15 @@
+const isLocalDevelopment = ['localhost', '127.0.0.1'].includes(window.location.hostname)
+
 window.appConfig = {
-    api: {
-        baseUrl: 'https://apps.indibit.eu/tba3-api',
-        inioApiUrl: 'https://api.inio.de/report_data_tba3',
-        inioAuthApiUrl: 'https://api.inio.de/auth',
-    },
+    api: isLocalDevelopment
+        ? {
+              baseUrl: '/api-proxy',
+              inioApiUrl: '/api-inio',
+              inioAuthApiUrl: '/api-auth',
+          }
+        : {
+              baseUrl: 'https://apps.indibit.eu/tba3-api',
+              inioApiUrl: 'https://api.inio.de/report_data_tba3',
+              inioAuthApiUrl: 'https://api.inio.de/auth',
+          },
 }
